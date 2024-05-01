91b486a721fea873a232a8e63c6ad0270f804875
kimkyungsub <cyon256@icloud.com>
2024-05-02 07:47:01 +0900

    gause sum

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,38 +1,15 @@
 
 package main;
 
-import java.util.Scanner;
-
 /**
  * Main
  */
 public class Main {
   public static void main(String[] args) {
-    Scanner stdIn = new Scanner(System.in);
-    System.out.println("find greates number from 3 inputs");
-    System.out.print("a: ");
-    int a = stdIn.nextInt();
-    System.out.print("b: ");
-    int b = stdIn.nextInt();
-    System.out.print("c: ");
-    int c = stdIn.nextInt();
+    int n = 5;
 
-    System.out.print("d: ");
-    int d = stdIn.nextInt();
+    int sum = (1 + n) * (n / 2) + ((n + 1) / 2);
 
-    int min = a;
-
-    if (b < min) {
-      min = b;
-    }
-    if (c < min) {
-      min = c;
-    }
-
-    if (d < min) {
-      min = d;
-    }
-    System.out.printf("smallest number is %d%n", min);
-    stdIn.close();
+    System.out.println(5 / 2);
   }
 }
