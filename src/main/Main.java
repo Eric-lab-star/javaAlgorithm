e0427cb7a51970a44266e89cf430c3c6a2a862c0
kimkyungsub <cyon256@icloud.com>
2024-05-02 08:20:19 +0900

    rest

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,11 +5,5 @@ package main;
  * Main
  */
 public class Main {
-  public static void main(String[] args) {
-    int n = 5;
-
-    int sum = (1 + n) * (n / 2) + ((n + 1) / 2);
-
-    System.out.println(5 / 2);
-  }
+  public static void main(String[] args) {}
 }
