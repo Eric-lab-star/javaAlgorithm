75238559f0d320fc330e34b47e2d124240eabfac
kimkyungsub <cyon256@icloud.com>
2024-05-04 14:45:01 +0900

    optimized for loop

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,21 @@
 
 package main;
 
+import java.util.Scanner;
+
 /**
  * Main
  */
 public class Main {
   public static void main(String[] args) {
-    int a = 0;
-    System.out.println((Object) a instanceof String);
+    Scanner stdIn = new Scanner(System.in);
+    int n = stdIn.nextInt();
+    for (int i = 0; i < n / 2; i++) {
+      System.out.println("+-");
+    }
+    if (n % 2 != 0) {
+      System.out.println("+");
+    }
+    stdIn.close();
   }
 }
