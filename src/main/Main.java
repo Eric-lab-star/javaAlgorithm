a4fbc22908f17ec69ca778f8916774b605ead643
kimkyungsub <cyon256@icloud.com>
2024-05-06 15:29:34 +0900

    tree

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,10 +14,18 @@ public class Main {
         System.out.print("size of triangle: ");
         n = stdIn.nextInt();
       } while (n <= 0);
+      int base = (n - 1) * 2 + 1;
 
       for (int x = 1; x <= n; x++) {
-        for (int y = 1; y <= x; y++) {
+        int target = (x - 1) * 2 + 1;
+        for (int y = 1; y <= (base - target) / 2; y++) {
+          System.out.print(" ");
+        }
+        for (int y = 1; y <= ((x - 1) * 2 + 1); y++) {
           System.out.print('*');
+        }
+        for (int y = 1; y <= (base - target) / 2; y++) {
+          System.out.print(" ");
         }
         System.out.println();
       }
