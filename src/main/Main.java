4e2cc362711070edb5eade4b30d640188f084439
kimkyungsub <cyon256@icloud.com>
2024-05-07 06:24:25 +0900

    swap

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 
 package main;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -8,27 +9,33 @@ import java.util.Scanner;
  */
 public class Main {
   public static void main(String[] args) {
-    int n;
+    int len, n;
     try (Scanner stdIn = new Scanner(System.in)) {
       do {
-        System.out.print("size of triangle: ");
-        n = stdIn.nextInt();
-      } while (n <= 0);
-      int base = (n - 1) * 2 + 1;
+        System.out.print("length of an Array: ");
+        len = stdIn.nextInt();
+      } while (len <= 0);
 
-      for (int x = 1; x <= n; x++) {
-        int target = (x - 1) * 2 + 1;
-        for (int y = 1; y <= (base - target) / 2; y++) {
-          System.out.print(" ");
-        }
-        for (int y = 1; y <= ((x - 1) * 2 + 1); y++) {
-          System.out.print('*');
-        }
-        for (int y = 1; y <= (base - target) / 2; y++) {
-          System.out.print(" ");
-        }
-        System.out.println();
+      int[] nums = new int[len];
+      for (int i = 0; i < len; i++) {
+        System.out.printf("nums[%d]: ", i);
+        n = stdIn.nextInt();
+        nums[i] = n;
       }
+
+      for (int i = 0; i < len / 2; i++) {
+        swap(nums, i, len - 1 - i);
+      }
+
+      System.out.println(Arrays.toString(nums));
+
+      //
     }
+  }
+
+  static void swap(int[] nums, int a, int b) {
+    int tmp = nums[a];
+    nums[a] = nums[b];
+    nums[b] = tmp;
   }
 }
